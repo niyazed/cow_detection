ea4543b4292c8960b36eaae5878cdc800c54f411
Niyaz B. Hashem <niyazed@outlook.com>
2019-09-06 15:36:37 +0600

    need to train

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -2,10 +2,10 @@
 Usage:
   # From tensorflow/models/
   # Create train data:
-  python generate_tfrecord.py --csv_input=data/train_labels.csv  --output_path=train.record
+  python generate_tfrecord.py --csv_input=data/train_labels.csv --image_dir=images/train --output_path=train.record
 
   # Create test data:
-  python generate_tfrecord.py --csv_input=data/test_labels.csv  --output_path=test.record
+  python generate_tfrecord.py --csv_input=data/test_labels.csv --image_dir=images/test --output_path=test.record
 """
 from __future__ import division
 from __future__ import print_function
--- xml_to_csv.py
+++ xml_to_csv.py
@@ -28,16 +28,16 @@ def xml_to_csv(path):
 # def main():
 #     image_path = os.path.join(os.getcwd(), 'annotations')
 #     xml_df = xml_to_csv(image_path)
-#     xml_df.to_csv('cow_labels.csv', index=None)
+#     xml_df.to_csv('data/cow_labels.csv', index=None)
 #     print('Successfully converted xml to csv.')
 
 
 def main():
-    for folder in ['train', 'test']:
-        image_path = os.path.join(os.getcwd(), ('images/' + folder))
-        xml_df = xml_to_csv(image_path)
-        xml_df.to_csv(('images/'+folder+'_labels.csv'), index=None)
-        print('Successfully converted xml to csv.')
+   for folder in ['train', 'test']:
+       image_path = os.path.join(os.getcwd(), ('annotations/' + folder))
+       xml_df = xml_to_csv(image_path)
+       xml_df.to_csv(('data/'+folder+'_labels.csv'), index=None)
+       print('Successfully converted xml to csv.')
 
 main()
 
